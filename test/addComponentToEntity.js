3d54a5c86643f58c6d50356680543e59294938b6
Pavel Vasev <pavel.vasev@gmail.com>
2021-07-17 22:11:36 +0500

    fix for issue 22

--- test/addComponentToEntity.js
+++ test/addComponentToEntity.js
@@ -25,3 +25,23 @@ ECS.addComponentToEntity(w3, e8, 'dupe')
 ECS.addComponentToEntity(w3, e8, 'dupe')
 
 tap.same(ECS.getEntities(w3, [ 'dupe' ], 'added'), [ e8 ], 'multiple additions only appear once in the added list')
+
+
+///////////////////////////////////////////
+// adding component to entity should not fire obsolete listeners
+// details discussed here https://github.com/mreinstein/ecs/issues/22
+{
+const w = ECS.createWorld()
+const entity = ECS.createEntity(w)
+
+tap.equal(ECS.getEntities(w, [ 'A','C' ],'added').length, 0)
+ECS.addComponentToEntity( w, entity, 'A')
+ECS.addComponentToEntity( w, entity, 'C')
+tap.equal(ECS.getEntities(w, [ 'A','C' ],'added').length, 1)
+ECS.cleanup( w );
+
+tap.equal(ECS.getEntities(w, [ 'A','C' ],'added').length, 0)
+ECS.removeComponentFromEntity( w, entity, 'A')
+ECS.addComponentToEntity( w, entity, 'B')
+tap.equal(ECS.getEntities(w, [ 'A','C' ],'added').length, 0)
+}
