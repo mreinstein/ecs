8025d584b6b4805498ee4fff5e47cad8234eab63
Jared Kozak <jared@kozan.dev>
2022-11-20 12:00:41 -0600

    add js doc to ordered insert

--- ordered-insert.js
+++ ordered-insert.js
@@ -1,4 +1,10 @@
-// insert the new item such that the array stays ordered from highest to lowest
+
+/**
+ * insert the new item such that the array stays ordered from highest to lowest
+ * @param {number[]} arr array of numbers to be mutated
+ * @param {number} val value to insert
+ * @returns {void} arr is mutated
+ */
 export default function orderedInsert (arr, val) {
     for (let i=0; i < arr.length; i++) {
         if (arr[i] <= val) {
