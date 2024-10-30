b58f6c6be637c158923c2771e6265ffcf6e4c4f9
Mike <reinstein.mike@gmail.com>
2024-10-30 14:35:22 -0700

    remove unused stuff

--- types/ordered-insert.d.ts
+++
@@ -1,8 +0,0 @@
-/**
- * insert the new item such that the array stays ordered from highest to lowest
- * @param {number[]} arr array of numbers to be mutated
- * @param {number} val value to insert
- * @returns {void} arr is mutated
- */
-export default function orderedInsert(arr: number[], val: number): void;
-//# sourceMappingURL=ordered-insert.d.ts.map
